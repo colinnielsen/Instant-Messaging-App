780875735fb7403fb375d4241aafc91730eb80d0
rives nielsen <colinrnielsen@gmail.com>
2020-08-26 22:13:06 -0600

    boiler plate

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Display3 } from 'baseui/typography';
+import { Card } from 'baseui/card';
 
 const App = () => {
    return (
-      <div>
-         hi
-      </div>
+      <Card>
+         <Display3>shelshack chatz</Display3>
+         {/* Sidebar */}
+         {/* Messages */}
+      </Card>
    )
 }
 
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+   apiKey: "AIzaSyDevuIPNxUt9B7V9gt2egw8MOLt9vaHlsg",
+   authDomain: "instant-messaging-app-f910f.firebaseapp.com",
+   databaseURL: "https://instant-messaging-app-f910f.firebaseio.com",
+   projectId: "instant-messaging-app-f910f",
+   storageBucket: "instant-messaging-app-f910f.appspot.com",
+   messagingSenderId: "993112960307",
+   appId: "1:993112960307:web:7464f72b517b1e015f6246",
+   measurementId: "G-MLHZ0T8X4F"
+};
